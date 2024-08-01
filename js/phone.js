46fb6912a7fcdd10e4af2d8fce6a829493462188
ashyxia <asifptw12@gmail.com>
2024-08-01 21:51:02 +0600

    Item descripiton is added through api calls and many modification is needed

--- js/phone.js
+++ js/phone.js
@@ -1,26 +1,27 @@
-const loadPhone = async (searchText) => {
+const loadPhone = async (searchText, isShowAll) => {
   const res = await fetch(
     `https://openapi.programming-hero.com/api/phones?search=${searchText}`
   );
   const data = await res.json();
   const phones = data.data;
-  displayPhones(phones);
+  displayPhones(phones, isShowAll);
 };
 
-const displayPhones = (phones) => {
+const displayPhones = (phones,isShowAll) => {
   const phoneContainer = document.getElementById("phone-container");
   phoneContainer.textContent = "";
 
 // display show all button if there are more than 12 phones
-const showAllContainer = document.getElementById('show-all-button');
-  if(phones.length>12){
+const showAllContainer = document.getElementById('show-all-container');
+  if(phones.length>12 && !isShowAll){
     showAllContainer.classList.remove('hidden');
   } else{
     showAllContainer.classList.add('hidden');
   }
   // display only 12 phones
+if(!isShowAll){
   phones = phones.slice(0,12);
-
+}
   phones.forEach((phone) => {
     console.log(phone);
     const phoneCard = document.createElement("div");
@@ -31,9 +32,9 @@ const showAllContainer = document.getElementById('show-all-button');
     </figure>
      <div class="card-body items-center text-center">
        <h2 class="card-title">${phone.phone_name}</h2>
-       <p>If a dog chews shoes whose shoes does he choose?</p>
+       <p>Latest Model & Faster</p>
        <div class="card-actions">
-         <button class="btn btn-primary">Buy Now</button>
+         <button onclick="handleShowDetail('${phone.slug}')" class="btn btn-primary">Show Details</button>
        </div>
       </div>               
     `;
@@ -42,13 +43,35 @@ const showAllContainer = document.getElementById('show-all-button');
   //  Hide loading spinner
   toggleLoadingSpinner(false);
 };
+//show details
+const handleShowDetail = async (id) => {
+  console.log("show details", id);
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`);
+  const data = await res.json();
+  const phone = data.data;
+  const showDetailContainer = document.getElementById('show-detail-container');
+  showDetailContainer.innerHTML = `
+    <img src="${phone.image}" alt=""/>
+    <h2>${phone.name}</h2>
+    <p><span>Storage:</span>${phone?.mainFeatures?.storage}</p>
+    <p><span>Display Size:</span>${phone?.mainFeatures?.displaySize}</p>
+  `
+  showPhoneDetails(phone);
+}
+const showPhoneDetails = (phone) =>{
+  console.log(phone);
+  const phoneName = document.getElementById('show-detail-phone-name');
+  phoneName.innerText = phone.name;
+  show_details_modal.showModal();
+}
+
 // Search Handler Button
-const handleSearch = () => {
+const handleSearch = (isShowAll) => {
   toggleLoadingSpinner(true);
   const searchField = document.getElementById("search-field");
   const searchText = searchField.value;
   console.log(searchText);
-  loadPhone(searchText);
+  loadPhone(searchText, isShowAll);
 };
 
 const toggleLoadingSpinner = (isLoading) =>{
@@ -58,4 +81,8 @@ const toggleLoadingSpinner = (isLoading) =>{
   } else {
     loadingSpinner.classList.add('hidden');
   }
+}
+// Handle show all
+const handleShowAll = () =>{
+    handleSearch(true);
 }
